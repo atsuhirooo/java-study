43c28f81be9fdda9b430b762c7dc7d4bfea7daa2
岸本 充弘 <atsuhiro.kishimoto@nts.ricoh.co.jp>
2020-11-13 16:10:54 +0900

    commit

--- js8ri/src/ch03/ex05/GrayTransform.java
+++ js8ri/src/ch03/ex05/GrayTransform.java
@@ -1,10 +1,20 @@
 package ch03.ex05;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public final class GrayTransform {
+public final class GrayTransform extends Application {
 
 	private static final int FRAME_SIZE = 10;
 
@@ -21,8 +31,12 @@ public final class GrayTransform {
 		return out;
 	}
 
-	public static void main(String[] args) {
-		Image image = new Image(args[0]);
+	public void start(Stage stage) {
+
+		Parameters params = getParameters();
+		List<String> args = params.getRaw();
+
+		Image image = new Image(args.get(0));
 
 		int width = (int) image.getWidth();
 		int height = (int) image.getHeight();
@@ -36,6 +50,22 @@ public final class GrayTransform {
 			return c;
 		});
 
+		BufferedImage resultImg = new BufferedImage((int) image.getWidth(), (int) image.getHeight(),
+				BufferedImage.TYPE_INT_ARGB);
+		SwingFXUtils.fromFXImage(transformedImage, resultImg);
+
+		// ファイル書き出し
+		try {
+			ImageIO.write(resultImg, "png", new File("new.png"));
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
 	}
 
 }
--- js8ri/src/ch03/ex06/BrightenTransform.java
+++ js8ri/src/ch03/ex06/BrightenTransform.java
@@ -1,12 +1,21 @@
 package ch03.ex06;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.function.BiFunction;
 
+import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public final class BrightenTransform {
+public final class BrightenTransform extends Application {
 
 	public static <T> Image transform(Image in, BiFunction<Color, T, Color> biFunction, T arg) {
 
@@ -21,13 +30,32 @@ public final class BrightenTransform {
 		return out;
 	}
 
-	public static void main(String[] args) {
+	public void start(Stage stage) {
 
-		Image image = new Image(args[0]);
+		Parameters params = getParameters();
+		List<String> args = params.getRaw();
+
+		Image image = new Image(args.get(0));
 		Image transformedImage = transform(image, (c, factor) -> {
 			return c.deriveColor(0, 1, factor, 1);
-		}, Double.valueOf(args[1]));
+		}, Double.valueOf(args.get(1)));
 
+		BufferedImage resultImg = new BufferedImage((int) image.getWidth(), (int) image.getHeight(),
+				BufferedImage.TYPE_INT_ARGB);
+		SwingFXUtils.fromFXImage(transformedImage, resultImg);
+
+		// ファイル書き出し
+		try {
+			ImageIO.write(resultImg, "png", new File("ex06.png"));
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
 	}
 
 }
